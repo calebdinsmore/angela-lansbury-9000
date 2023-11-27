39ff059c7e2f89caf21d4d2cc89008eb20f102ab
Caleb Dinsmore <calebjdinsmore@gmail.com>
2023-11-26 22:34:05 -0500

    Added ability to mass DM server owners who use Angela

--- bot/cogs/admin/admin_commands.py
+++ bot/cogs/admin/admin_commands.py
@@ -1,7 +1,9 @@
 import nextcord
-from nextcord import slash_command, Interaction
+import sentry_sdk
+from nextcord import slash_command, Interaction, SlashOption
 from nextcord.ext import commands
 
+from bot.utils import messages
 from bot.utils.constants import TESTING_GUILD_ID
 
 
@@ -18,3 +20,33 @@ class AdminCommands(commands.Cog):
         guilds = '\n'.join([f'- {g.name}' for g in self.bot.guilds])
         embed.add_field(name='Servers', value=guilds, inline=False)
         await interaction.send(embed=embed)
+
+    @slash_command(name='owner-blast', description='Send a message to all server owners using Angela.',
+                   guild_ids=[TESTING_GUILD_ID])
+    async def owner_blast(self,
+                          interaction: Interaction,
+                          confirmation: str = SlashOption(name='confirmation',
+                                                          choices=['Test', 'Send']),
+                          message: str = SlashOption(name='message')):
+        await interaction.response.defer()
+        if confirmation == 'Test':
+            channel = self.bot.get_channel(interaction.channel_id)
+            await interaction.send('Sending message as test.')
+            await channel.send(message)
+            return
+
+        failures = []
+        owner_name = None
+        sent_to = set()
+        try:
+            for guild in self.bot.guilds:
+                if guild.owner_id in sent_to:
+                    continue
+                owner_name = guild.owner.name
+                await guild.owner.send(message)
+                sent_to.add(guild.owner_id)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            failures.append(owner_name)
+
+        await interaction.send('Sent mass message.', embed=messages.error(f'Failures: {failures}'))
