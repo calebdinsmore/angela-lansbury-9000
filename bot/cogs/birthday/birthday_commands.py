06fdb90cb32c23af50f351eafc2d872ea36ae0ba
Brittany Ellich <brittanyellich@github.com>
2023-12-13 09:48:40 -0800

    Add date validation

--- bot/cogs/birthday/birthday_commands.py
+++ bot/cogs/birthday/birthday_commands.py
@@ -27,14 +27,17 @@ class BirthdayCommands(commands.Cog):
                             date: str = SlashOption(name='date', description='Date of birthday (MM/DD/YYYY)')):
         if interaction.guild is not None and interaction.guild_id not in [BUMPERS_GUILD_ID, TESTING_GUILD_ID]:
             return await interaction.send(embed=messages.error('Birthday tracking is unsupported in this server.'))
-        # Get date from added date, TODO: add validation
-        dt = datetime.strptime(date, '%m/%d/%Y')
-        success = add_birthday(user.id, interaction.guild_id, name, dt.month, dt.day, dt.year)
-        if success:
-            await interaction.send('Birthday added.')
-        else:
-            await interaction.send(f'Birthday already exists for {name}. Try a different name, or remove the existing birthday first.')
-
+        # Get date from added date, validate, and add to the database. Return an error if the name already exists or the date is invalid.
+        try:
+            dt = datetime.strptime(date, '%m/%d/%Y')
+            success = add_birthday(user.id, interaction.guild_id, name, dt.month, dt.day, dt.year)
+            if success:
+                await interaction.send('Birthday added.')
+            else:
+                await interaction.send(f'Birthday already exists for {name}. Try a different name, or remove the existing birthday first.')
+        except ValueError:
+            return await interaction.send('Please provide a date in the format MM/DD/YYYY')
+        
     @birthday.subcommand(name='list', description='List birthdays')
     async def birthday_list(self, interaction: Interaction,
                             user: Member = SlashOption(name='user',
