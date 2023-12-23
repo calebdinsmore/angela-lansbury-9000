a3b0ffc939bc444ed274a146f745a6d87456a060
Caleb Dinsmore <calebjdinsmore@gmail.com>
2023-12-23 10:57:21 -0500

    Change date format and make bday commands ephemeral

--- bot/cogs/birthday/birthday_commands.py
+++ bot/cogs/birthday/birthday_commands.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone, time
 import asyncio
 
+import sentry_sdk
 from nextcord import slash_command, Interaction, SlashOption, Permissions, Member, TextChannel
 from nextcord.ext import commands, tasks
 
@@ -9,11 +10,12 @@ from bot.utils import messages
 from bot.utils.constants import TESTING_GUILD_ID, BUMPERS_GUILD_ID
 from db.helpers import birthday_helper
 
+
 class BirthdayCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.post_birthdays.start()
-    
+
     # Every day at 5AM Pacific Time
     @tasks.loop(time=time(hour=12, minute=0, second=0, tzinfo=timezone.utc))
     async def post_birthdays(self):
@@ -60,8 +62,8 @@ class BirthdayCommands(commands.Cog):
 
     @birthday_admin.subcommand(name='config', description='Configure birthday settings')
     async def birthday_config(self, interaction: Interaction,
-                                channel: TextChannel = SlashOption(name='channel',
-                                                                description='Channel to post birthday messages in.')):
+                              channel: TextChannel = SlashOption(name='channel',
+                                                                 description='Channel to post birthday messages in.')):
         success = birthday_helper.update_settings(interaction.guild_id, channel.id)
         if not success:
             return await interaction.send(f'An error occurred when updating birthday settings.')
@@ -78,41 +80,46 @@ class BirthdayCommands(commands.Cog):
 
     @birthday.subcommand(name='add', description='Add a birthday')
     async def birthday_add(self, interaction: Interaction,
-                            name: str = SlashOption(name='name', description='Name of person celebrating a birthday'),
-                            date: str = SlashOption(name='date', description='Date of birthday (MM/DD/YYYY)')):
+                           name: str = SlashOption(name='name', description='Name of person celebrating a birthday'),
+                           date: str = SlashOption(name='date', description='Date of birthday (YYYY/MM/DD)')):
         # Get date from added date, validate, and add to the database. Return an error if the name already exists or the date is invalid.
         try:
-            dt = datetime.strptime(date, '%m/%d/%Y')
-            success = birthday_helper.add_birthday(interaction.guild_id, interaction.user.id, name.lower(), dt.month, dt.day, dt.year)
+            dt = datetime.strptime(date, '%Y/%m/%d')
+            success = birthday_helper.add_birthday(interaction.guild_id, interaction.user.id, name.lower(), dt.month,
+                                                   dt.day, dt.year)
             if success:
-                await interaction.send('Birthday added.')
+                await interaction.send(f'Birthday added: `{name} | {dt.strftime("%b %d, %Y")}`', ephemeral=True)
             else:
-                await interaction.send(f'Birthday already exists for {name.title()}. Try a different name, or remove the existing birthday first.')
+                await interaction.send(f'Birthday already exists for {name.title()}. '
+                                       f'Try a different name, or remove the existing birthday first.',
+                                       ephemeral=True)
         except ValueError:
-            return await interaction.send('Please provide a date in the format MM/DD/YYYY')
-        
+            return await interaction.send('Please provide a date in the format YYYY/MM/DD', ephemeral=True)
+
     @birthday.subcommand(name='list', description='List birthdays')
     async def birthday_list(self, interaction: Interaction,
                             user: Member = SlashOption(name='user',
-                                                         description='User to list birthdays for.'),):
+                                                       description='User to list birthdays for.'), ):
         birthdays = birthday_helper.list_birthdays(interaction.guild_id, user.id)
         if len(birthdays) == 0:
             embed = messages.info(f'No stored birthdays found for {user.name}. Use `/birthday add` to add a birthday.')
             icon_url = user.avatar.url if user.avatar else None
             embed.set_author(name=user.name, icon_url=icon_url)
-            return await interaction.send(embed=embed)
-        embed = messages.info(f'Current birthdays stored for {user.name}. Use `/birthday remove` to remove a stored birthday.')
+            return await interaction.send(embed=embed, ephemeral=True)
+        embed = messages.info(
+            f'Current birthdays stored for {user.name}. Use `/birthday remove` to remove a stored birthday.')
         icon_url = user.avatar.url if user.avatar else None
         embed.set_author(name=user.name, icon_url=icon_url)
         for birthday in birthdays:
-            embed.add_field(name=birthday[0].name.title(), value=f'{birthday[0].month}/{birthday[0].day}/{birthday[0].year}')
+            embed.add_field(name=birthday[0].name.title(),
+                            value=f'{birthday[0].month}/{birthday[0].day}/{birthday[0].year}')
         await interaction.send(embed=embed)
 
     @birthday.subcommand(name='remove', description='Remove a birthday')
     async def birthday_remove(self, interaction: Interaction,
-                                name: str = SlashOption(name='name', description='Name of birthday to remove')):
+                              name: str = SlashOption(name='name', description='Name of birthday to remove')):
         success = birthday_helper.delete_birthday(interaction.guild_id, interaction.user.id, name.lower())
         if not success:
-            return await interaction.send(f'An error occurred when deleting birthday {name.title()} associated with user {user.name}.')
-        await interaction.send('Successfully deleted birthday!')
-        
+            return await interaction.send(f'An error occurred when deleting birthday {name.title()} '
+                                          f'associated with user {interaction.user.display_name}.', ephemeral=True)
+        await interaction.send('Successfully deleted birthday!', ephemeral=True)
