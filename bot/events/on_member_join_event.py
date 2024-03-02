6e4504da175dd4c97e68e55d44f5efa1725796f2
Caleb Dinsmore <calebjdinsmore@gmail.com>
2024-03-02 14:08:57 -0500

    Removed temp print and gated user activity setup to only the bumpers guild

--- bot/events/on_member_join_event.py
+++ bot/events/on_member_join_event.py
@@ -1,6 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 
+from bot.utils.constants import BUMPERS_GUILD_ID
 from db.helpers import user_activity_helper
 
 
@@ -8,6 +9,8 @@ def register_event(bot: commands.Bot):
     @bot.event
     async def on_member_join(member: nextcord.Member):
         if member.bot:
+            return
+        if member.guild.id != BUMPERS_GUILD_ID:
             return
 
         user_activity_helper.setup_user(member.id, member.guild.id)
