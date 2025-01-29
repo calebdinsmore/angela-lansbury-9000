eb62f6213856af64133c09cbc245bb23685fecac
Caleb Dinsmore <calebjdinsmore@gmail.com>
2025-01-28 21:23:23 -0500

    added log on guild join

--- bot/events/on_guild_join_event.py
+++ bot/events/on_guild_join_event.py
@@ -2,13 +2,18 @@ import nextcord
 import sentry_sdk
 from nextcord.ext import commands
 
+from bot.utils.logger import get_logger, LoggingLevel
+
 
 def register_event(bot: commands.Bot):
     @bot.event
     async def on_guild_join(guild: nextcord.Guild):
         try:
-            await guild.owner.send('👋 Thanks for adding me! If you have questions or concerns, or just want to be '
-                                   'updated on new developments or issues, feel free to join '
-                                   'https://discord.gg/NQm54zS82w')
+            logger = get_logger(LoggingLevel.GENERAL)
+            await logger.log(f'Joined guild `{guild.name}`')
+            if guild.owner:
+                await guild.owner.send('👋 Thanks for adding me! If you have questions or concerns, or just want to be '
+                                       'updated on new developments or issues, feel free to join '
+                                       'https://discord.gg/NQm54zS82w')
         except Exception as e:
             sentry_sdk.capture_exception(e)
